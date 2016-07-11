ec39cf6481707cb08a088cd02ccc924e50188a87
Ernesto Alfonso <erjoalgo@gmail.com>
2016-07-10 19:55:49 -0700

    great erjoalgo-compile-mode

--- lisp/settings/compile-settings.el
+++ lisp/settings/compile-settings.el
@@ -1,24 +1,103 @@
-(defun compile-set-command-and-run (cmd)
-  (interactive (list (read-shell-command "enter compile command: " compile-command)))
-  (setf compile-command cmd)
+(defun erjoalgo-compile-compile (arg)
+  (interactive "P")
+  (when arg
+    (call-interactively 'erjoalgo-compile-prompt-and-set-command))
+  (let ((cmd-list
+	 (or (erjoalgo-compile-read-cmd-list)
+	     (erjoalgo-compile-cmd-for-buffer (current-buffer)))))
+    (when (functionp cmd-list)
+      (setf cmd-list (list cmd-list)))
+    (if (or arg (not cmd-list))
+	(recompile)
+      (loop for cmd in
+	    ;(if (atom cmd-list) (list cmd-list) cmd-list)
+	    cmd-list
+	    do
+	    (cond
+	     ((stringp cmd) (compile cmd))
+	     ((functionp cmd) (funcall cmd (current-buffer)))
+	     (t (error "cmd must be function or string, not %s" cmd)))))))
+
+(defun erjoalgo-compile-read-cmd-list ()
+  ;;(read-file-local-variable-value 'compile-command)
+  ;;TODO read file local compile-command
+  )
+
+(defun erjoalgo-compile-cmd-for-buffer (buffer)
+  (loop for matcher in erjoalgo-compile-cmd-for-buffer
+	thereis (funcall matcher buffer)))
+
+(defvar erjoalgo-compile-cmd-for-buffer ()
+  "list of functions, each should return
+the command for compiling a particular buffer,
+or nil if unknown")
+
+(defun erjoalgo-compile-prompt-and-set-command (cmd)
+  (interactive
+   (list (read-shell-command "enter compile command: " compile-command)))
   (add-file-local-variable 'compile-command compile-command)
-  (compile compile-command))
+  (setf compile-command cmd)
+  ;(compile compile-command)
+  )
+
+(defun wrap-ignore (fun)
+  (lexical-let ((fun fun))
+    (lambda (&rest args) (funcall fun))))
+
+(setf erjoalgo-compile-cmd-for-buffer
+      ;;TODO make this less verbose
+      (list
+       (lambda (buff)
+	 (when (-> (buffer-local-value 'major-mode buff)
+		   (eq 'fundamental-mode))
+	   ;;git commit
+	   (wrap-ignore 'server-edit)))
+
+       (lambda (buff)
+	 (when (-> (buffer-local-value 'major-mode buff)
+		   (eq 'sh-mode))
+	   (format "bash %s" (f-filename (buffer-file-name)))))
+
+       (lambda (buff)
+	 (when (-> (buffer-local-value 'major-mode buff)
+		   (eq 'java-mode))
+	   (let ((f-no-ext
+		  (-> (buffer-file-name) (f-filename) (f-no-ext))))
+	     (format "javac %s.java && java %s" f-no-ext f-no-ext))))
+
+       (lambda (buff)
+	 (when (-> (buffer-local-value 'major-mode buff)
+		   (eq 'c-mode))
+	   (let ((fn (f-filename (buffer-file-name))))
+	     (format "gcc -g %s && ./a.out" fn fn))))
+
+       (lambda (buff)
+	 (when (-> (buffer-local-value 'major-mode buff)
+		   (eq 'go-mode))
+	   "go test"))
+
+       (lambda (buff)
+	 (when (-> (buffer-local-value 'major-mode buff)
+		   (eq 'tex-mode))
+	   latex-compile))
+
+       (lambda (buff)
+	 (when (-> (buffer-local-value 'major-mode buff)
+		   (eq 'python-mode))
+	   (format "python %s" (f-filename (buffer-file-name)))))
+))
 
 
-;(global-set-key (kbd "M-c") 'compile)
-;(global-set-key (kbd "M-c") 'recompile)
-(global-set-key (kbd "M-c") 'recompile)
-(global-set-key (kbd "M-C") 'compile)
-;(global-set-key (kbd "M-C") 'compile-set-command-and-run)
-(setf compilation-ask-about-save nil )
-;(setf compilation-read-command t)
-(setf compilation-read-command nil)
+
+(global-set-key (kbd "M-c") 'erjoalgo-compile-compile)
+(global-set-key (kbd "M-C") (lambda () (interactive)
+			      (erjoalgo-compile-compile '(4))))
+
+(setf compilation-ask-about-save nil)
+;;(setf compilation-read-command nil)
 
 (global-set-key (kbd "M-p") 'previous-error)
 (global-set-key (kbd "M-n") 'next-error)
-
-;(add-hook 'go-mode-hook (lambda () (setf compile-command "go run *.go")))
-(add-hook 'go-mode-hook (lambda () (setf compile-command "go test")))
 
 
 ;taken from
@@ -47,21 +126,6 @@
 
 (add-to-list 'compilation-finish-functions
 	     'compilation-finished-notify)
-
-(with-eval-after-load "tex-mode"
-  (define-key tex-mode-map (kbd "M-c") 'latex-compile))
-
-;(defvar mode-to-compile-command
-(setf mode-to-compile-command
- '((sh-mode (format "bash %s" (f-filename (buffer-file-name))))
-  (python-mode (format "python %s" (f-filename (buffer-file-name))))
-  (java (let (f-no-ext (f-filename (f-no-ext (buffer-file-name))))
-	  (format "javac %s.java && java %s" f-no-ext f-no-ext)))))
-
-(loop for (mode form) in mode-to-compile-command do
-  (add-hook-to-modes
-       `(lambda () (setf compile-command ,form))
-       (list mode)))
 
 ;;TODO autoload recompile
 (require 'compile)
