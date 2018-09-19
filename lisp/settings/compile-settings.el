c5d40c8ab88e98edb160df3bec13ada07db1855d
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-19 11:46:29 -0700

    opt to interpret ansi-color escape sequences
    in the compilation buffer

--- lisp/settings/compile-settings.el
+++ lisp/settings/compile-settings.el
@@ -30,3 +30,17 @@
   (set-process-query-on-exit-flag proc nil))
 
 (add-hook 'compilation-start-hook 'erjoalgo-compile-disable-query-on-proc-exit)
+
+
+(defvar compilation-interpret-ansi-color nil)
+
+(setf compilation-interpret-ansi-color t);; todo make buffer-local and mode-local
+
+(defun maybe-colorize-compilation-buffer ()
+  ;; (require 'ansi-color)
+  ;; https://stackoverflow.com/questions/3072648/
+  (when compilation-interpret-ansi-color
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region compilation-filter-start (point)))))
+
+(add-hook 'compilation-filter-hook 'maybe-colorize-compilation-buffer)
