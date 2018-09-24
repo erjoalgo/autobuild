2f787f3dc18140b6373d4ee2614aa55112d6f68c
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-24 00:41:18 -0700

    safe-wrap notification to avoid compilation-finish-functions crash

--- lisp/settings/compile-settings.el
+++ lisp/settings/compile-settings.el
@@ -2,27 +2,28 @@
   "if non-nil, do not notify of compilation ended if compilation took less than
  ‘erjoalgo-compile-notify-threshold-secs' seconds")
 
-(setf erjoalgo-compile-notify-min-compilation-duration 0)
+(setf erjoalgo-compile-notify-min-compilation-duration 10)
 
 (defun erjoalgo-compile-post-compile-message (compilation-buffer compilation-state)
   ;; TODO try notify-send, xmessage, audible/visible beep...
-  (when compilation-state
-    (message "compilation %s" (s-trim compilation-state))
-    (when (and
-           ;; no remaining cmds in the pieline
-           (null erjoalgo-compile-command-queue)
-           ;; this fails when emacs is not raised and therefore not visible...
-           ;; (not (frame-visible-p (selected-frame)))
-           (or (null erjoalgo-compile-notify-min-compilation-duration)
-               (>=
-                (- (time-to-seconds) erjoalgo-compile-last-compilation-start-time)
-                erjoalgo-compile-notify-min-compilation-duration))
-           (not (member (emacs-pid) (stumpwm-visible-window-ids t))))
-      (stumpwm-message
-       (let ((red "^1") (green "^2"))
-         (format "%scompilation %s^*"
-                 (if (equal "finished" (s-trim compilation-state)) green red)
-                 compilation-state))))))
+  (safe-wrap
+   (when compilation-state
+     (message "compilation %s" (s-trim compilation-state))
+     (when (and
+            ;; no remaining cmds in the pieline
+            (null erjoalgo-compile-command-queue)
+            ;; this fails when emacs is not raised and therefore not visible...
+            ;; (not (frame-visible-p (selected-frame)))
+            (or (null erjoalgo-compile-notify-min-compilation-duration)
+                (>=
+                 (- (time-to-seconds) erjoalgo-compile-last-compilation-start-time)
+                 erjoalgo-compile-notify-min-compilation-duration))
+            (not (member (emacs-pid) (stumpwm-visible-window-ids t))))
+       (stumpwm-message
+        (let ((red "^1") (green "^2"))
+          (format "%scompilation %s^*"
+                  (if (equal "finished" (s-trim compilation-state)) green red)
+                  compilation-state)))))))
 
 (add-hook 'erjoalgo-compile-pipeline-finished-hook 'erjoalgo-compile-post-compile-message)
 
