25a6ca9b444543e9c28ed3c90c9d817aa6058039
Ernesto Alfonso <erjoalgo@gmail.com>
2019-03-01 21:26:51 -0800

    Revert "inline continuation lambda"
    This reverts commit fd27966f70c9a1e36d62560a8d8c2798be02999e.

--- autobuild.el
+++ autobuild.el
@@ -196,17 +196,28 @@
   (when (bound-and-true-p autobuild-pipeline-rules-remaining-vvar)
     (autobuild-process-add-sentinel
      proc
-     `(lambda (proc finish-state)
+     `(lambda (buffer state)
         (defvar autobuild-pipeline-rules-remaining-vvar)
         (let ((autobuild-pipeline-rules-remaining-vvar
                ',autobuild-pipeline-rules-remaining-vvar))
-          (when rules
-            (if (autobuild-compilation-exited-abnormally-p finish-state)
-                (progn
-                  (message "aborting pipeline: %s" rules))
-              (progn
-                (message "continuing with pipeline: %s" rules)
-                (autobuild-pipeline-run nil)))))))))
+          (autobuild-pipeline-continue
+           buffer
+           state
+           autobuild-pipeline-rules-remaining-vvar))))))
+
+(defun autobuild-pipeline-continue (proc finish-state rules)
+  "Internal.  Used to resume an asynchronous pipeline.
+
+   COMPILATION-BUFFER FINISH-STATE are the arguments passed
+   to functions in ‘compilation-finish-functions'."
+  (when rules
+    (if (autobuild-compilation-exited-abnormally-p finish-state)
+        (progn
+          (message "aborting pipeline: %s" rules))
+      (progn
+        (message "continuing with pipeline: %s" rules)
+        (autobuild-pipeline-run nil)))))
+
 
 (add-hook 'compilation-start-hook #'autobuild-pipeline-setup-continuation)
 
