53f6ef2a1eed37d4acbf97b2760e8c4356091e86
Ernesto Alfonso <erjoalgo@gmail.com>
2019-06-12 19:47:57 -0700

    raise file-local-compile rule priority if file-local var exists

--- autobuild-examples.el
+++ autobuild-examples.el
@@ -54,8 +54,10 @@
 
 (autobuild-define-rule autobuild-file-local-compile-command nil
   "Set and run the file-local compile command"
-  (autobuild-nice 12)
   (when (buffer-file-name)
+    (autobuild-nice
+       (if (alist-get 'compile-command file-local-variables-alist)
+           8 12))
     (lambda ()
       (let* ((command
               (if (and (bound-and-true-p compile-command)
