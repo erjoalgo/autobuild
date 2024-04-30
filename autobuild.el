ffe9fafddf2e1527c8f5aaf645130af66b025f7b
Ernesto Alfonso <erjoalgo@gmail.com>
2024-04-30 10:08:59 -0400

    bump selcand dependency version

--- autobuild.el
+++ autobuild.el
@@ -7,7 +7,7 @@
 
 ;; Created: Wed Jan 23 20:45:01 2019 (-0800)
 ;; Version: 0.2.2
-;; Package-Requires: ((emacs "26.1") (selcand "0.0.1"))
+;; Package-Requires: ((emacs "26.1") (selcand "0.0.3"))
 ;; URL: https://github.com/erjoalgo/autobuild
 ;; Keywords: compile, build, pipeline, autobuild, extensions, processes, tools
 ;; Compatibility:
